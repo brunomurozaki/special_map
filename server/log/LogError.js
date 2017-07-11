0900e347722ad333a8665bfffae1eeaf05f861ef
Bruno Murozaki <brunoimurozaki@gmail.com>
2017-07-11 14:09:56 -0300

    Bug fix on error log

--- server/log/LogError.js
+++ server/log/LogError.js
@@ -2,7 +2,14 @@ const fs = require('fs');
 var path = require('path');
 
 module.exports = function(error){
-	fs.appendFile(path.join(__dirname + '/../../public/logs/error.txt'), error + "\n", function(err) {
+	var logsPath = '/../../public/logs';
+	
+	if(!fs.existsSync(__dirname + logsPath)){
+		console.log("aaaa");
+		fs.mkdirSync(path.join(__dirname + logsPath));
+	}
+	
+	fs.appendFile(path.join(__dirname + logsPath +'/error.txt'), error + "\n", function(err) {
 	    if(err) {
 	        return console.log(err);
 	    }
